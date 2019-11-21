260ecffbf34a0ca95ee4a5b3f634c7bc7f7c9c72
David Porcheron <david.porcheron@ekino.com>
2019-11-21 18:22:21 +0100

    fix(duplicateTask): Prevent recreating compose extension and tasks while applying plugin

--- src/main/kotlin/com/ekino/oss/gradle/plugin/docker/DockerPlugin.kt
+++ src/main/kotlin/com/ekino/oss/gradle/plugin/docker/DockerPlugin.kt
@@ -1,8 +1,9 @@
 package com.ekino.oss.gradle.plugin.docker
 
-import com.avast.gradle.dockercompose.ComposeSettings
+import com.avast.gradle.dockercompose.ComposeExtension
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.Task
 import org.gradle.api.tasks.JavaExec
 import org.gradle.api.tasks.testing.Test
 import java.io.File
@@ -10,38 +11,41 @@ import java.io.File
 class DockerPlugin : Plugin<Project> {
 
   override fun apply(project: Project) {
-    // plugins
-    project.pluginManager.apply("docker-compose")
+    with(project) {
+      if (!File("${projectDir}/docker-compose.yml").exists()) {
+        return
+      }
 
-    if (File("${project.projectDir}/docker-compose.yml").exists()) {
-      val dockerCompose = ComposeSettings(project)
+      // plugins
+      pluginManager.apply("docker-compose")
+
+      val dockerCompose = extensions.getByType(ComposeExtension::class.java)
       dockerCompose.forceRecreate = true
       dockerCompose.removeOrphans = true
       dockerCompose.upAdditionalArgs = listOf("--renew-anon-volumes")
 
-      with(project) {
-        afterEvaluate {
-          // Run integration tests using docker compose
-          with(task("integrationTest")) {
-            dependsOn("composeUp")
-
-            doFirst {
-              dockerCompose.exposeAsEnvironment(it as Test)
-            }
-
-            finalizedBy("composeDown")
+      afterEvaluate {
+        // Run integration tests using docker compose
+        applyConfigForTask("integrationTest") {
+          dependsOn("composeUp")
+          doFirst {
+            dockerCompose.exposeAsEnvironment(it as Test)
           }
+          finalizedBy("composeDown")
+        }
 
-          // Local launching with PostgreSQL database
-          with(task("bootRun")) {
-            dependsOn("composeUp")
-
-            doFirst {
-              dockerCompose.exposeAsEnvironment(it as JavaExec)
-            }
+        // Local launching with docker compose
+        applyConfigForTask("bootRun") {
+          dependsOn("composeUp")
+          doFirst {
+            dockerCompose.exposeAsEnvironment(it as JavaExec)
           }
         }
       }
     }
+  }
+
+  private inline fun Project.applyConfigForTask(taskName: String, config: Task.() -> Unit) {
+    getTasksByName(taskName, false).firstOrNull()?.config()
   }
 }
--- src/test/kotlin/com/ekino/oss/gradle/plugin/docker/DockerPluginTest.kt
+++ src/test/kotlin/com/ekino/oss/gradle/plugin/docker/DockerPluginTest.kt
@@ -1,19 +1,87 @@
 package com.ekino.oss.gradle.plugin.docker
 
-import org.gradle.api.Project
+import com.avast.gradle.dockercompose.ComposeExtension
+import org.gradle.api.internal.tasks.DefaultTaskDependency
 import org.gradle.testfixtures.ProjectBuilder
-import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertFalse
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.io.TempDir
+import java.nio.file.Files
+import java.nio.file.Path
 
 class DockerPluginTest {
 
-  @Test
-  fun `should contain docker-compose plugin`() {
-    val project: Project = ProjectBuilder.builder().build()
-    assertEquals(0, project.plugins.size)
+  @TempDir
+  lateinit var tempDir: Path
 
-    project.pluginManager.apply("com.ekino.oss.gradle.plugin.docker")
+  @Test
+  fun `should contain docker-compose plugin and proper defined tasks`() {
+    // Given
+    Files.write(tempDir.resolve("docker-compose.yml"), "".toByteArray())
+
+    val project = ProjectBuilder.builder()
+        .withProjectDir(tempDir.toFile())
+        .build()
+    project.task("integrationTest")
+    project.task("bootRun")
+
+    // When
+    project.pluginManager.apply(DockerPlugin::class.java)
+
+    // Then
     assertTrue(project.plugins.hasPlugin("docker-compose"))
+
+    val dockerComposeExtension = project.extensions.getByType(ComposeExtension::class.java)
+    assertTrue(dockerComposeExtension.forceRecreate)
+    assertTrue(dockerComposeExtension.removeOrphans)
+    assertTrue(dockerComposeExtension.upAdditionalArgs.contains("--renew-anon-volumes"))
+
+    // Force the project to be evaluated
+    val bootRunTask = project.getTasksByName("bootRun", false).first()
+    assertTrue(bootRunTask.dependsOn.contains("composeUp"))
+
+    val integrationTestTask = project.getTasksByName("integrationTest", false).first()
+    assertTrue(integrationTestTask.dependsOn.contains("composeUp"))
+    assertTrue((integrationTestTask.finalizedBy as DefaultTaskDependency).mutableValues.contains("composeDown"))
+  }
+
+  @Test
+  fun `should contain docker-compose plugin without specific tasks`() {
+    // Given
+    Files.write(tempDir.resolve("docker-compose.yml"), "".toByteArray())
+
+    val project = ProjectBuilder.builder()
+        .withProjectDir(tempDir.toFile())
+        .build()
+
+    // When
+    project.pluginManager.apply(DockerPlugin::class.java)
+
+    // Then
+    assertTrue(project.plugins.hasPlugin("docker-compose"))
+
+    val dockerComposeExtension = project.extensions.getByType(ComposeExtension::class.java)
+    assertTrue(dockerComposeExtension.forceRecreate)
+    assertTrue(dockerComposeExtension.removeOrphans)
+    assertTrue(dockerComposeExtension.upAdditionalArgs.contains("--renew-anon-volumes"))
+
+    val tasksNames = project.getAllTasks(false).getValue(project).map { it.name }
+    assertFalse(tasksNames.contains("bootRun"))
+    assertFalse(tasksNames.contains("integrationTest"))
+  }
+
+  @Test
+  fun `should not contain docker-compose plugin without docker-compose file`() {
+    // Given
+    val project = ProjectBuilder.builder()
+        .withProjectDir(tempDir.toFile())
+        .build()
+
+    // When
+    project.pluginManager.apply(DockerPlugin::class.java)
+
+    // Then
+    assertFalse(project.plugins.hasPlugin("docker-compose"))
   }
 }
